6adec7e776d4e34f83bbb521a4327572ce6ea967
Przemysław Górecki <gorecki@ermlab.com>
2017-04-04 13:40:00 +0200

    add home controller and view

---
+++ src/main/java/com/ermlab/giflib/controller/GifController.java
@@ -0,0 +1,19 @@
+package com.ermlab.giflib.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class GifController {
+    @RequestMapping("/")
+    public String listGifs() {
+        return "home";
+    }
+
+    @RequestMapping("/gif")
+    @ResponseBody
+    public String showGif() {
+        return "A GIF!!";
+    }
+}
